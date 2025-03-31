20ef140f187efaccdba29653d73ffcee5ca9e1fe
49rohan <142834725+49rohan@users.noreply.github.com>
2025-03-30 23:16:36 -0500

    Steps 16-18

---
+++ ClassScheduleTests/TeacherControllerTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Moq;
+using ClassSchedule.Models;
+using ClassSchedule.Controllers;
+
+namespace ClassScheduleTests
+{
+    public class TeacherControllerTests
+    {
+        [Fact]
+        public void IndexActionMethod_ReturnsAViewResult()
+        {
+
+        }
+    }
+}
